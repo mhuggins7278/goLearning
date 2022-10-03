483ca274ff2c9775115d9fc802a67fb8cd04834a
Mark Huggins <mhuggins@glgroup.com>
2022-10-03 15:48:28 -0400

    refactor finished

--- hello.go
+++ hello.go
@@ -2,16 +2,31 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
 
-func Hello(name string) string {
-  if name == "" {
-    name = "World"
+func greetingPrefix(language string) (prefix string) {
+  switch language {
+  case french:
+    prefix = frenchHelloPrefix
+  case spanish:
+    prefix = spanishHelloPrefix
+  default:
+    prefix = englishHelloPrefix
   }
-    return englishHelloPrefix + name
+  return
 }
 
 func main() {
-    fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
